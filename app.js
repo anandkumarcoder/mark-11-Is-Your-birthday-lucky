7c9f3daa2e99c75b697531f41c26e4a60ee1d260
anandkumarcoder <akanand104@gmail.com>
2021-08-14 02:25:30 +0530

    query selector done

---
+++ app.js
@@ -0,0 +1,7 @@
+const UserDateOfBirth = document.querySelector(".datofBirth");
+
+const userLuckyNumber = document.querySelector("#luckyNumber");
+
+const checkButton = document.querySelector("#CheckButton")
+
+const outputDiv =document.querySelector(".output")
