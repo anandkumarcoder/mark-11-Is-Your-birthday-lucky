83e8f7837586d9fb99025bba88b041be8a2f9cb6
anandkumarcoder <akanand104@gmail.com>
2021-08-14 05:59:46 +0530

    replacing char, adding dob,comparing values

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-const UserDateOfBirth = document.querySelector(".datofBirth");
+const userDateOfBirth = document.querySelector("#dateOfBirth");
 
 const userLuckyNumber = document.querySelector("#luckyNumber");
 
-const checkButton = document.querySelector("#CheckButton")
+const checkButton = document.querySelector("#checkButton")
 
 const outputDiv =document.querySelector(".output")
+
+checkButton.addEventListener ('click' ,  checkBirthDateIsLucky)
+
+
+
+function checkBirthDateIsLucky(){
+    const dob = userDateOfBirth.value;
+    const sum = calculateSum(dob);
+    compareValues(sum,userLuckyNumber.value)
+
+}
+
+
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-",""); //replacing charcter
+    let sum = 0;
+    for( let index=0; index<dob.length; index++){
+        sum = sum + Number(dob.charAt(index));
+        //converting to strings to number
+        //0+number of character in dob  
+    }
+return sum;
+}
+
+
+function compareValues(sum,userLuckyNumber){
+    if(sum%userLuckyNumber===0){
+        console.log("your birthday is lucky");
+    } else {
+        console.log("Sorry your birthday is not lucky")
+    }
+}
